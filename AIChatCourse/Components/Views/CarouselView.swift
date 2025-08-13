60e6c3ec2dd85fe943edaa386cd661de2a2ab367
josiei <jocelyn.icaza@gmail.com>
2025-08-13 09:47:49 -0700

    [Feature] AI Carousel View

---
+++ AIChatCourse/Components/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  AIChatCourse
+//
+//  Created by Jocelyn Icaza on 8/12/25.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
