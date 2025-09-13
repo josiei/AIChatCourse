b6432c6b0a454e33e013e584b40920e6ff7412ee
josiei <jocelyn.icaza@gmail.com>
2025-09-13 10:52:44 -0700

    [Feature] CreateCharacterView

---
+++ AIChatCourse/Components/Views/AsyncCallToActionButton.swift
@@ -0,0 +1,52 @@
+//
+//  AsyncCallToActionButton.swift
+//  AIChatCourse
+//
+//  Created by Jocelyn Icaza on 9/12/25.
+//
+
+import SwiftUI
+
+struct AsyncCallToActionButton: View {
+    var isLoading: Bool = false
+    var title: String = "Save"
+    var action: () -> Void
+    
+    var body: some View {
+        ZStack {
+            if isLoading {
+                ProgressView()
+                    .tint(.white)
+            } else {
+                Text(title)
+            }
+        }
+        .callToActionButton()
+        .anyButton(.press) {
+            action()
+        }
+        .disabled(isLoading)
+    }
+}
+
+private struct PreviewView: View {
+    @State private var isLoading: Bool = false
+    var body: some View {
+        AsyncCallToActionButton(
+            isLoading: isLoading,
+            title: "Finish",
+            action: {
+                isLoading = true
+                Task {
+                    try? await Task.sleep(for: .seconds(3))
+                    isLoading = false
+                }
+            }
+        )
+    }
+}
+
+#Preview {
+    PreviewView()
+        .padding()
+}
---
+++ AIChatCourse/Services/Avatar/Models/AvatarAttributes.swift
@@ -0,0 +1,62 @@
+//
+//  AvatarAttributes.swift
+//  AIChatCourse
+//
+//  Created by Jocelyn Icaza on 9/13/25.
+//
+
+enum CharacterOption: String, CaseIterable, Hashable {
+    case man, woman, alien, dog, cat
+    
+    static var `default`: Self {
+        .man
+    }
+    
+    var startsWithVowel: Bool {
+        switch self {
+        case .alien:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+enum CharacterAction: String, CaseIterable, Hashable {
+    case smiling, studying, eating, shopping, crying, fighting, walking, relaxing, sitting, drinking
+    
+    static var `default`: Self {
+        .smiling
+    }
+}
+
+enum CharacterLocation: String, CaseIterable, Hashable {
+    case park, mall, museum, city, dessert, forrest, space
+    
+    static var `default`: Self {
+        .park
+    }
+}
+
+struct AvatarDescriptionBuilder {
+    let characterOption: CharacterOption
+    let characterAction: CharacterAction
+    let characterLocation: CharacterLocation
+    
+    init(characterOption: CharacterOption, characterAction: CharacterAction, characterLocation: CharacterLocation) {
+        self.characterOption = characterOption
+        self.characterAction = characterAction
+        self.characterLocation = characterLocation
+    }
+    
+    init(avatar: AvatarModel) {
+        self.characterOption = avatar.characterOption ?? .default
+        self.characterAction = avatar.characterAction ?? .default
+        self.characterLocation = avatar.characterLocation ?? .default
+    }
+    
+    var characterDescription: String {
+        let prefix = characterOption.startsWithVowel ? "An" : "A"
+        return "\(prefix) \(characterOption.rawValue) that is \(characterAction.rawValue) in the \(characterLocation.rawValue)"
+    }
+}
