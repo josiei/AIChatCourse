04cccbf67c81010ef2f31bdd40c191a995732eda
josiei <jocelyn.icaza@gmail.com>
2025-09-21 13:06:05 -0700

    [Feature] ChatBubbleView

---
+++ AIChatCourse/Core/Chat/ChatView.swift
@@ -0,0 +1,44 @@
+//
+//  ChatView.swift
+//  AIChatCourse
+//
+//  Created by Jocelyn Icaza on 9/14/25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
+    @State private var avatar: AvatarModel? = .mock
+    @State private var currentUser: UserModel? = .mock
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(chatMessages) { message in
+                        let isCurrentUser = message.authorId == currentUser?.userId
+                        ChatBubbleViewBuilder(
+                            message: message,
+                            isCurrentUser: isCurrentUser,
+                            imageName: avatar?.profileImageName
+                        )
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(8)
+            }
+            
+            Rectangle()
+                .frame(height: 50)
+        }
+        .navigationTitle(avatar?.name ?? "Chat")
+        .toolbarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ChatView()
+    }
+}
